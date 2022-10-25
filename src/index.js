b1514f012f089bfb29a018f72c592d79b55fe985
Kótai Dániel <dkotai66@gmail.com>
2022-10-25 14:26:57 +0200

    Feladat befejezve

--- src/index.js
+++ src/index.js
@@ -5,19 +5,83 @@ var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (
     if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
     return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
 };
-var _football_golokSzama;
+var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
+    return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
+};
+var _football_aCsapatGol, _football_bCsapatGol, _Calvinball_winner;
 class football {
-    constructor(winner, golokszama, date) {
-        _football_golokSzama.set(this, void 0);
-        this.winner = winner;
-        __classPrivateFieldSet(this, _football_golokSzama, golokszama, "f");
+    constructor(aCsapat, aCsapatGol, bCsapat, bCsapatGol, date) {
+        _football_aCsapatGol.set(this, void 0);
+        _football_bCsapatGol.set(this, void 0);
+        __classPrivateFieldSet(this, _football_aCsapatGol, aCsapatGol, "f");
+        this.aCsapat = aCsapat;
+        this.bCsapat = bCsapat;
+        __classPrivateFieldSet(this, _football_bCsapatGol, bCsapatGol, "f");
         this.date = date;
+        if (aCsapatGol > bCsapatGol) {
+            this.winner = aCsapat;
+        }
+        else {
+            this.winner = bCsapat;
+        }
     }
     result() {
         console.log(this.toString());
     }
     toString() {
-        return "Football match: 6-3";
+        return "Football match: " + __classPrivateFieldGet(this, _football_aCsapatGol, "f") + "-" + __classPrivateFieldGet(this, _football_bCsapatGol, "f");
     }
 }
-_football_golokSzama = new WeakMap();
+_football_aCsapatGol = new WeakMap(), _football_bCsapatGol = new WeakMap();
+class marathon {
+    constructor(winner, date, futottIdo) {
+        this.winner = winner;
+        this.date = date;
+        this.futottIdo = futottIdo;
+    }
+    result() {
+        console.log(this.toString());
+    }
+    toString() {
+        return "Marathon: " + Math.floor(this.futottIdo / 60) + " min " + this.futottIdo % 60 + " s ";
+    }
+}
+class Calvinball {
+    constructor(winner, date) {
+        _Calvinball_winner.set(this, void 0);
+        __classPrivateFieldSet(this, _Calvinball_winner, winner, "f");
+        this.date = date;
+        this.veletlenEgeszSzam = Math.floor(Math.random() * 90) + 11;
+    }
+    get winner() {
+        return __classPrivateFieldGet(this, _Calvinball_winner, "f");
+    }
+    set winner(winner) {
+        if (this.winner == "Calvin" || this.winner == "Hobbes") {
+            __classPrivateFieldSet(this, _Calvinball_winner, winner, "f");
+        }
+        else {
+            __classPrivateFieldSet(this, _Calvinball_winner, "Error", "f");
+        }
+    }
+    result() {
+        console.log(this.toString());
+    }
+    toString() {
+        return "Calvinball: " + this.veletlenEgeszSzam + " points";
+    }
+}
+_Calvinball_winner = new WeakMap();
+let eredmenyek = [
+    new football('xxxx', 5, 'yyyyy', 3, new Date("2022-05-05")),
+    new football('zzzz', 3, 'ttttt', 7, new Date("2022-05-06")),
+    new marathon('Pepsi béla', new Date("2022-04-01"), 118),
+    new marathon('CocaCola béla', new Date("2022-04-03"), 130),
+    new Calvinball('hhhhh', new Date("2022-06-06")),
+    new Calvinball('Calvin', new Date("2022-07-10")),
+];
+for (let eredmeny of eredmenyek) {
+    eredmeny.result();
+}
--- src/index.ts
+++ src/index.ts
@@ -7,21 +7,101 @@ interface Results{
 
 class football implements Results{
     winner: string;
-    #golokSzama: number;
+    #aCsapatGol: number;
+    #bCsapatGol: number;
+    aCsapat : string;
+    bCsapat : string;
     date: Date;
 
-    constructor(winner: string, golokszama: number, date: Date){
-        this.winner = winner;
-        this.#golokSzama = golokszama;
+    constructor(aCsapat : string, aCsapatGol: number, bCsapat : string, bCsapatGol: number, date: Date){
+        this.#aCsapatGol = aCsapatGol;
+        this.aCsapat = aCsapat;
+        this.bCsapat = bCsapat;  
+        this.#bCsapatGol = bCsapatGol;
         this.date = date;
+
+        if(aCsapatGol > bCsapatGol){
+            this.winner = aCsapat;
+        }
+        else{
+            this.winner = bCsapat;
+        }
     }
 
+    
 
     result(): void {
         console.log(this.toString());
     }
 
     toString(): string {
-        return "Football match: 6-3";   
+        return "Football match: " + this.#aCsapatGol + "-" + this.#bCsapatGol;   
     }
+}
+
+class marathon implements Results{
+    winner: string;
+    date: Date;
+    futottIdo : number;
+
+    constructor(winner: string, date: Date, futottIdo: number){
+        this.winner = winner;
+        this.date = date;
+        this.futottIdo = futottIdo;
+    }
+
+    result(): void {
+        console.log(this.toString());
+    }
+    toString(): string {
+        return "Marathon: " + Math.floor(this.futottIdo/60) + " min " + this.futottIdo%60 + " s " 
+    }
+
+}
+
+class Calvinball implements Results{
+    #winner: string;
+    date: Date;
+    veletlenEgeszSzam: number;
+
+    constructor(winner:string, date:Date){
+        this.#winner = winner;
+        this.date = date;
+        this.veletlenEgeszSzam = Math.floor(Math.random() * 90) + 11;
+    } 
+
+    get winner() {
+        return this.#winner;
+    }
+
+    set winner(winner: string) {
+        if (this.winner == "Calvin" || this.winner == "Hobbes") {
+            this.#winner = winner;
+        }
+        else {
+            this.#winner = "Error"
+        }
+    }
+
+    result(): void {
+        console.log(this.toString());
+    }
+
+    toString() : string{
+        return "Calvinball: " + this.veletlenEgeszSzam + " points";
+    }
+}
+
+let eredmenyek: Results[] = [
+    new football ('xxxx', 5, 'yyyyy', 3, new Date("2022-05-05")),
+    new football ('zzzz', 3, 'ttttt', 7, new Date("2022-05-06")),
+    new marathon ('Pepsi béla', new Date("2022-04-01"), 118),
+    new marathon ('CocaCola béla', new Date("2022-04-03"), 130),
+    new Calvinball ('hhhhh', new Date("2022-06-06" )),
+    new Calvinball ('Calvin', new Date("2022-07-10" )),
+
+]
+
+for(let eredmeny of eredmenyek){
+    eredmeny.result();
 }
